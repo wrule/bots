de599ddc1f0cac0372391a0f905c303efcf4267b
jimao <jimao@jimaodeMacBook-Pro.local>
2022-12-02 11:39:07 +0800

    止盈止损

--- sma_cross.ts
+++ sma_cross.ts
@@ -41,8 +41,19 @@ extends Bot<OHLCV, Signal> {
     return result;
   }
 
+  private stop_loss = -0.1;
+  private stop_profit = 0.1;
+
   protected exec(signal: Signal) {
     if (!signal.closed) this.queue.pop();
+    if (this.executor.Risk(signal.close) < this.stop_loss) {
+      // 止损
+      this.executor.SellAll(0);
+    }
+    if (this.executor.Risk(signal.close) > this.stop_profit) {
+      // 止盈
+      this.executor.SellAll(0);
+    }
     if (signal.sell) {
       this.executor.SellAll(signal.close);
     } else if (signal.buy) {
