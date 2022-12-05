4aae07237a256d2c81a5ae551d6784aa0a9852ef
jimao <jimao@jimaodeMacBook-Pro.local>
2022-12-05 15:57:59 +0800

    红眼回测

--- example/backtesting.ts
+++ example/backtesting.ts
@@ -1,17 +1,17 @@
 #!/usr/bin/env node
 import { ArrayToKLine, ExpandKLine, SpotSimpleTest } from 'litebot';
-import { SMACross } from '../sma_cross';
+import { StochRSICross } from '../stoch_rsi_cross';
 
-const data = require('../data/ETH_USDT-2h.json');
+const data = require('../data/ETH_USDT-30m.json');
 
 function main() {
-  const kline = ExpandKLine(ArrayToKLine(data, false), 0.5);
+  const kline = ArrayToKLine(data, false);
   const executor = new SpotSimpleTest();
-  const bot = new SMACross(executor, {
-    fast_period: 9,
-    slow_period: 44,
-    stop_rate: 1,
-    take_rate: 1e6,
+  const bot = new StochRSICross(executor, {
+    rsi_period: 13,
+    k_period: 32,
+    d_period: 45,
+    stoch_period: 45,
   });
   bot.BackTestingBatch(kline);
   console.log(executor.ROI(kline[kline.length - 1].close));
