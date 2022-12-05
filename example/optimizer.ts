40a0c497f062c60bfed69e7ccaba3039df7761db
jimao <jimao@jimaodeMacBook-Pro.local>
2022-12-05 15:18:12 +0800

    策略优化

--- example/optimizer.ts
+++ example/optimizer.ts
@@ -2,17 +2,17 @@
 import { ArrayToKLine, Random, SpotSimpleTest } from 'litebot';
 import { MAKD, Params } from '../makd';
 
-const data = require('../data/BTC_USDT-30m.json');
+const data = require('../data/BTC_USDT-15m.json');
 
 function main() {
   const kline = ArrayToKLine(data, false);
   const random = new Random<Params>();
   random.Search({
     domain: {
-      fast_period: [20, 40],
-      slow_period: [50, 70],
-      k_period: [2, 20],
-      d_period: [2, 20],
+      fast_period: [2, 50],
+      slow_period: [10, 60],
+      k_period: [2, 50],
+      d_period: [2, 50],
     },
     target: (params) => {
       const executor = new SpotSimpleTest();
