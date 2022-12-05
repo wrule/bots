04045a34b3dc45f0cd38463df50aa2f12a822c32
jimao <jimao@jimaodeMacBook-Pro.local>
2022-12-05 14:13:09 +0800

    btc优化

--- index.ts
+++ index.ts
@@ -1,2 +1,10 @@
+import { ArrayToKLine } from 'litebot';
 
-console.log('你好，世界');
+const data = require('./data/BTC_USDT-30m.json');
+
+async function main() {
+  const kline = ArrayToKLine(data);
+  console.log('你好，世界');
+}
+
+main();
