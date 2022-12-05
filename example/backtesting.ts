3ce196e8adcd8320eac5784cfc22f2f23c6bdb81
jimao <jimao@jimaodeMacBook-Pro.local>
2022-12-05 15:45:29 +0800

    回测研究

--- example/backtesting.ts
+++ example/backtesting.ts
@@ -1,10 +1,20 @@
 #!/usr/bin/env node
-import { ArrayToKLine } from 'litebot';
+import { ArrayToKLine, ExpandKLine, SpotSimpleTest } from 'litebot';
+import { SMACross } from '../sma_cross';
 
 const data = require('../data/ETH_USDT-2h.json');
 
 function main() {
-  const kline = ArrayToKLine(data);
+  const kline = ExpandKLine(ArrayToKLine(data, false), 0.5);
+  const executor = new SpotSimpleTest();
+  const bot = new SMACross(executor, {
+    fast_period: 9,
+    slow_period: 44,
+    stop_rate: 0.03,
+    take_rate: 0.05,
+  });
+  bot.BackTestingBatch(kline);
+  console.log(executor.ROI(kline[kline.length - 1].close));
 }
 
 main();
