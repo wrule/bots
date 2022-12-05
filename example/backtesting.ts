57d4afef8268b97ced8e0ec2ffe5b2c05d6c0f3d
jimao <jimao@jimaodeMacBook-Pro.local>
2022-12-05 23:18:03 +0800

    提交代码

--- example/backtesting.ts
+++ example/backtesting.ts
@@ -6,11 +6,11 @@ import { Clams } from '../clams';
 const data = require('../data/ETH_USDT-1d.json');
 
 function main() {
-  const kline = ArrayToKLine(data, false);
+  const kline = ExpandKLine(ArrayToKLine(data, false), 0.5);
   const executor = new SpotSimpleTest();
   const bot = new Clams(executor, {
-    stop_rate: 1,
-    take_rate: 1e6,
+    stop_rate: 0.02,
+    take_rate: 0.2,
   });
   bot.BackTestingBatch(kline);
   console.log(executor.ROI(kline[kline.length - 1].close));
