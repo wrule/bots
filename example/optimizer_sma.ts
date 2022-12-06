490ba2cc8bcfa93aa9e219face1393f069cc3738
jimao <jimao@jimaodeMacBook-Pro.local>
2022-12-06 23:15:02 +0800

    另一个优化IQ

--- example/optimizer_sma.ts
+++ example/optimizer_sma.ts
@@ -1,22 +1,20 @@
 #!/usr/bin/env node
 import { ArrayToKLine, Random, SpotSimpleTest } from 'litebot';
-import { StochRSICross, Params } from '../stoch_rsi_cross';
+import { SMACross, Params } from '../sma_cross';
 
-const data = require('../data/BTC_USDT-30m.json');
+const data = require('../data/BTC_USDT-1d.json');
 
 function main() {
   const kline = ArrayToKLine(data, false);
   const random = new Random<Params>();
   random.Search({
     domain: {
-      rsi_period: [5, 13],
-      stoch_period: [35, 43],
-      k_period: [31, 39],
-      d_period: [40, 48],
+      fast_period: [9, 9],
+      slow_period: [44, 44],
     },
     target: (params) => {
       const executor = new SpotSimpleTest();
-      const bot = new StochRSICross(executor, params);
+      const bot = new SMACross(executor, params);
       bot.BackTestingBatch(kline);
       return executor.ROI(kline[kline.length - 1].close);
     },
