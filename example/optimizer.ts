74415c4c28599ed46fce539f63fad5098d2a78f8
jimao <jimao@jimaodeMacBook-Pro.local>
2022-12-06 22:52:28 +0800

    优化器

--- example/optimizer.ts
+++ example/optimizer.ts
@@ -1,22 +1,22 @@
 #!/usr/bin/env node
 import { ArrayToKLine, Random, SpotSimpleTest } from 'litebot';
-import { MAKD, Params } from '../makd';
+import { StochRSICross, Params } from '../stoch_rsi_cross';
 
-const data = require('../data/BTC_USDT-15m.json');
+const data = require('../data/BTC_USDT-30m.json');
 
 function main() {
   const kline = ArrayToKLine(data, false);
   const random = new Random<Params>();
   random.Search({
     domain: {
-      fast_period: [2, 50],
-      slow_period: [10, 60],
-      k_period: [2, 50],
-      d_period: [2, 50],
+      rsi_period: [5, 13],
+      stoch_period: [35, 43],
+      k_period: [31, 39],
+      d_period: [40, 48],
     },
     target: (params) => {
       const executor = new SpotSimpleTest();
-      const bot = new MAKD(executor, params);
+      const bot = new StochRSICross(executor, params);
       bot.BackTestingBatch(kline);
       return executor.ROI(kline[kline.length - 1].close);
     },
