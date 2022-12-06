17962b740b053e7492b232633dafdd3f0bd8838b
jimao <jimao@jimaodeMacBook-Pro.local>
2022-12-06 15:07:46 +0800

    belong

--- index.ts
+++ index.ts
@@ -2,23 +2,33 @@ import { ArrayToKLine, OHLCV } from 'litebot';
 
 const data = require('./data/ETH_USDT-1d.json');
 
-function fk(kline: OHLCV[]) {
+function extract(kline: OHLCV[]) {
   const max = Math.max(...kline.map((item) => item.high));
   const min = Math.min(...kline.map((item) => item.low));
   const diff = max - min;
   const level = (num: number) => Math.floor((num - min) / diff * 10);
-  const points: [number, number][] = [];
+  const result: [number, number][] = [];
   kline.forEach((item, index) => {
-    points.push([level(item.low), level(item.high)]);
-    if (index === kline.length - 1) points.push([level(item.close), NaN]);
+    result.push([level(item.low), level(item.high)]);
+    if (index === kline.length - 1) result.push([level(item.close), level(item.close)]);
   });
-  console.log(points);
+  return result;
+}
+
+function belong(kline: OHLCV[], pattern: [number, number][]) {
+  if (kline.length + 1 !== pattern.length) return false;
+  const sample = extract(kline);
+  return pattern.every((item, index) => {
+    const low = Math.min(...item);
+    const high = Math.max(...item);
+    return sample[index][0] >= low && sample[index][1] <= high;
+  });
 }
 
 async function main() {
   const kline = ArrayToKLine(data);
   console.log('你好，世界');
-  fk(kline.splice(kline.length - 10));
+  console.log(fk(kline.splice(kline.length - 10)));
 }
 
 main();
