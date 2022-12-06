ab6c4b13a4a37b8bee54081027e96726135fb602
jimao <jimao@jimaodeMacBook-Pro.local>
2022-12-06 13:57:17 +0800

    裸k测试

--- example/backtesting.ts
+++ example/backtesting.ts
@@ -3,14 +3,14 @@ import { ArrayToKLine, ExpandKLine, SpotSimpleTest } from 'litebot';
 // import { StochRSICross } from '../stoch_rsi_cross';
 import { Clams } from '../clams';
 
-const data = require('../data/ETH_USDT-1d.json');
+const data = require('../data/ETH_USDT-1h.json');
 
 function main() {
-  const kline = ArrayToKLine(data);
+  const kline = ExpandKLine(ArrayToKLine(data, false), 0.5);
   const executor = new SpotSimpleTest();
   const bot = new Clams(executor, {
     stop_rate: 0.01,
-    take_rate: 1e6,
+    take_rate: 0.1,
   });
   bot.BackTestingBatch(kline);
   console.log(executor.ROI(kline[kline.length - 1].close));
