41a4e056a55e08f2063a712b24cf5cfdaea0c086
jimao <jimao@jimaodeMacBook-Pro.local>
2022-12-07 15:45:25 +0800

    提交代码

--- example/backtesting.ts
+++ example/backtesting.ts
@@ -1,16 +1,20 @@
 #!/usr/bin/env node
 import { ArrayToKLine, ExpandKLine, SpotSimpleTest } from 'litebot';
-// import { StochRSICross } from '../stoch_rsi_cross';
-import { Clams } from '../clams';
+import { StochRSICross } from '../stoch_rsi_cross';
+// import { Clams } from '../clams';
 
-const data = require('../data/ETH_USDT-5m.json');
+const data = require('../data/BTC_USDT-30m.json');
 
 function main() {
   const kline = ExpandKLine(ArrayToKLine(data, false), 0.5);
   const executor = new SpotSimpleTest();
-  const bot = new Clams(executor, {
-    stop_rate: 1,
-    take_rate: 1e6,
+  const bot = new StochRSICross(executor, {
+    rsi_period: 10,
+    stoch_period: 35,
+    k_period: 35,
+    d_period: 44,
+    stop_rate: 0.35,
+    take_rate: 0.1,
   });
   bot.BackTestingBatch(kline);
   console.log(executor.ROI(kline[kline.length - 1].close));
