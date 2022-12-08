f43b7b286b00e9d197317d13d9449218d28402b0
jimao <jimao@jimaodeMacBook-Pro.local>
2022-12-08 18:04:38 +0800

    提交代码

--- example/backtesting.ts
+++ example/backtesting.ts
@@ -5,14 +5,14 @@ import { MAKD } from '../makd';
 const data = require('../data/BTC_USDT-1h.json');
 
 function main() {
-  const kline = ArrayToKLine(data, false);
+  const kline = ExpandKLine(ArrayToKLine(data, false), 0.5);
   const executor = new SpotSimpleTest();
   const bot = new MAKD(executor, {
     fast_period: 15,
     slow_period: 29,
     k_period: 4,
     d_period: 7,
-    stop_rate: 0.25,
+    stop_rate: 0.1,
     take_rate: 1e6,
   });
   bot.BackTestingBatch(kline);
