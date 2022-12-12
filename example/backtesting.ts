7ffb39e7a7db02c3b90b87ee5a4d57a5c9fee70a
jimao <jimao@jimaodeMacBook-Pro.local>
2022-12-12 15:44:35 +0800

    依赖升级

--- example/backtesting.ts
+++ example/backtesting.ts
@@ -6,17 +6,18 @@ const data = require('../data/BTC_USDT-1h.json');
 
 function main() {
   const kline = ExpandKLine(ArrayToKLine(data, false), 0.5);
-  const executor = new SpotSimpleTest();
+  const executor = new SpotSimpleTest(100, -0.001);
   const bot = new MAKD(executor, {
     fast_period: 15,
     slow_period: 29,
     k_period: 4,
     d_period: 7,
-    stop_rate: 0.1,
+    stop_rate: 1,
     take_rate: 1e6,
   });
   bot.BackTestingBatch(kline);
   console.log(executor.ROI(kline[kline.length - 1].close));
+  console.log(executor.ROINet(kline[kline.length - 1].close));
 }
 
 main();
