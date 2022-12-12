8261014fe445bd981ddaed04c16b4be84a047096
jimao <jimao@jimaodeMacBook-Pro.local>
2022-12-12 16:32:07 +0800

    参数修改

--- rsikd.ts
+++ rsikd.ts
@@ -69,9 +69,12 @@ extends Bot<TC, Signal> {
     d_period: 4,
     stop_rate: 1,
     take_rate: 1e6,
-    interval: 500,
     funds: 15,
     assets: 0,
+    final_price: NaN,
+    last_action: '',
+    init_valuation: NaN,
+    interval: 500,
   };
   FillParams(params);
   const notifier = new DingTalk(secret.notifier);
