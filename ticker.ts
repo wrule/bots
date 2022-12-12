5b2966118f3ca79a64ef65d99a1da491f5a27d53
jimao <jimao@jimaodeMacBook-Pro.local>
2022-12-12 17:23:17 +0800

    ticker\

--- ticker.ts
+++ ticker.ts
@@ -2,8 +2,13 @@
 import { ccxt } from 'litebot';
 
 async function main() {
+  let symbols = process.argv.slice(2);
+  if (symbols.length < 1) symbols.push('BTC', 'ETH');
+  symbols = symbols
+    .map((item) => item.includes('/') ? item : `${item}/USDT`)
+    .map((item) => item.toUpperCase());
   const exchange = new ccxt.binance();
-  const tickers = await exchange.fetchTickers(['BTC/USDT', 'ETH/USDT']);
+  const tickers = await exchange.fetchTickers(symbols);
   Object.values(tickers).forEach((item) => {
     console.log(item.symbol, [item.bid, item.ask]);
   });
