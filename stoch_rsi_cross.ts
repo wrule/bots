49d93c93e0ff31c6349fa15a1150c1364f03a545
jimao <jimao@jimaodeMacBook-Pro.local>
2022-12-12 16:37:00 +0800

    参数调整

--- stoch_rsi_cross.ts
+++ stoch_rsi_cross.ts
@@ -87,10 +87,13 @@ extends Bot<TC, Signal> {
     d_period: 5,
     stop_rate: 1,
     take_rate: 1e6,
-    interval: 1000,
     funds: 15,
     assets: 0,
+    final_price: NaN,
+    last_action: '',
+    init_valuation: NaN,
     rt: true,
+    interval: 1000,
   };
   FillParams(params);
   const notifier = new DingTalk(secret.notifier);
