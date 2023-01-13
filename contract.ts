9d0990c82c218f98f6ce068d5915da9681ca3b74
jimao <jimao@jimaodeMacBook-Pro.local>
2023-01-13 18:55:24 +0800

    old

--- contract.ts
+++ contract.ts
@@ -3,6 +3,40 @@ import fs from 'fs';
 import { ccxt } from 'litebot';
 import HttpsProxyAgent from 'https-proxy-agent';
 
+
+async function old() {
+  const secret = require('../.secret.json');
+  const exchange = new ccxt.binance({
+    ...secret.exchange,
+    options: { defaultType: 'future', hedgeMode: true },
+  });
+  console.log('加载市场...');
+  await exchange.loadMarkets();
+  const rsp = await exchange.createMarketBuyOrder(
+    'BTC/USDT',
+    0.001,
+    {
+      positionSide: 'SHORT',
+      // quoteOrderQty: 40,
+    },
+  );
+  // const rsp = await exchange.createOrder(
+  //   'BTC/USDT',
+  //   'MARKET',
+  //   'buy',
+  //   20,
+  //   undefined,
+  //   {
+  //     positionSide: 'LONG',
+  //     quoteOrderQty:
+  //   },
+  // );
+  console.log(rsp);
+  fs.writeFileSync('output.json', JSON.stringify(rsp, null, 2));
+  console.log('你好，世界');
+}
+
+
 async function list(exchange: ccxt.Exchange) {
   const balance = await exchange.fetchFreeBalance();
   Object.entries(balance).forEach(([key, value]) => {
