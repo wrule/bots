f290955546501017828c8e41e72dabcd1472debd
jimao <jimao@jimaodeMacBook-Pro.local>
2023-01-13 17:28:20 +0800

    提交代码

--- spot.ts
+++ spot.ts
@@ -3,24 +3,31 @@ import fs from 'fs';
 import { ccxt } from 'litebot';
 import HttpsProxyAgent from 'https-proxy-agent';
 
+async function list(exchange: ccxt.Exchange) {
+  const balance = await exchange.fetchFreeBalance();
+  Object.entries(balance).forEach(([key, value]) => {
+    if (value > 0) console.log(key, value);
+  });
+}
+
 async function main() {
   const secret = require('./.secret.json');
   const exchange = new ccxt.binance({
     ...secret.exchange,
     agent: HttpsProxyAgent('http://127.0.0.1:7890'),
   });
-  const balance = await exchange.fetchFreeBalance();
-  Object.entries(balance).forEach(([key, value]) => {
-    if (value > 0) console.log(key, value);
-  });
-  console.log(1);
+  await list(exchange);
+  console.log('开始交易');
   const order = await exchange.createMarketOrder(
-    'NFT/USDT',
-    'sell',
-    5049240.176924,
+    'ETH/USDT',
+    'buy',
+    0,
+    undefined,
+    { quoteOrderQty: 11 },
   );
-  console.log(2);
+  console.log('结束交易，写入结果...');
   fs.writeFileSync('output.json', JSON.stringify(order, null, 2));
+  await list(exchange);
 }
 
 main();
