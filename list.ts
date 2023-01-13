738ca0693d8d4ceb5039b8661e7bb0f56eaa8405
jimao <jimao@jimaodeMacBook-Pro.local>
2023-01-13 16:12:45 +0800

    提交代码

--- list.ts
+++ list.ts
@@ -1,9 +1,13 @@
 #!/usr/bin/env node
 import { ccxt } from 'litebot';
+import HttpsProxyAgent from 'https-proxy-agent';
 
 async function main() {
   const secret = require('./.secret.json');
-  const exchange = new ccxt.binance(secret.exchange);
+  const exchange = new ccxt.binance({
+    ...secret.exchange,
+    agent: HttpsProxyAgent('http://127.0.0.1:7890'),
+  });
   const balance = await exchange.fetchFreeBalance();
   Object.entries(balance).forEach(([key, value]) => {
     if (value > 0) console.log(key, value);
