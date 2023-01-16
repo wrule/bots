fe46c5e1fac7f493674251c9a452f02727f302b6
jimao <jimao@jimaodeMacBook-Pro.local>
2023-01-16 17:22:03 +0800

    https-proxy-agent

--- list.ts
+++ list.ts
@@ -1,10 +1,15 @@
 #!/usr/bin/env node
 import { ExFactory } from 'litebot';
+import HttpsProxyAgent from 'https-proxy-agent';
 
 export
 async function list() {
   const secret = require('./.secret.json');
-  const exchange = ExFactory(secret.exchange);
+  const exchange = ExFactory({
+    ...secret.exchange,
+    agent: secret.exchange.https_proxy_agent ?
+      HttpsProxyAgent(secret.exchange.https_proxy_agent) : undefined,
+  });
   const balance = await exchange.fetchFreeBalance();
   Object.entries(balance).forEach(([key, value]) => {
     if (value > 0) console.log(key, value);
