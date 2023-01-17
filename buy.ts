ce2de027865dc8f2d0cd59a3d1439cd5a6fcf7bc
jimao <jimao@jimaodeMacBook-Pro.local>
2023-01-17 10:48:08 +0800

    交易的代码

--- buy.ts
+++ buy.ts
@@ -9,21 +9,34 @@ async function main() {
     agent: secret.exchange.https_proxy_agent ?
       HttpsProxyAgent(secret.exchange.https_proxy_agent) : undefined,
   });
+
   let symbol = process.argv[2];
   let amount_str = process.argv[3];
   if (symbol == null) throw 'missing symbol';
   if (amount_str == null) throw 'missing amount';
   symbol = (symbol.includes('/') ? symbol : `${symbol}/USDT`).toUpperCase();
+  const target = symbol.split('/')[0];
+  const source = symbol.split('/')[1];
   amount_str = amount_str.toUpperCase();
-  console.log(symbol, amount_str);
   let amount = amount_str.trim() ? Number(amount_str) : NaN;
   if (amount_str === 'ALL') {
     const balances = await exchange.fetchBalance();
-    balances[''].
-  } else {
-
+    amount = balances[source].free;
   }
+  console.log(symbol, amount, target, source);
   if (isNaN(amount)) throw 'amount illegal';
+
+
+  const order = await exchange.createMarketBuyOrder(
+    symbol,
+    exchange.costToPrecision(symbol, amount),
+    {
+      quoteOrderQty: exchange.id === 'binance' ?
+        exchange.costToPrecision(symbol, amount) : undefined,
+      tgtCcy: exchange.id === 'okx' ?
+        'quote_ccy' : undefined,
+    } as any,
+  );
 }
 
 main();
