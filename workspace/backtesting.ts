5fccb3b9d6949d6e5c6d4c3a2093e63bce66cddf
jimao <jimao@jimaodeMacBook-Pro.local>
2023-01-31 11:13:39 +0800

    回测代码修改

--- workspace/backtesting.ts
+++ workspace/backtesting.ts
@@ -1,23 +1,21 @@
 #!/usr/bin/env node
 import { ArrayToKLine, ExpandKLine, SpotSimpleTest } from 'litebot';
-import { MAKD } from '../makd';
+import { SMACross } from '../sma_cross';
 
-const data = require('../data/BTC_USDT-1h.json');
+const data = require('../data/BTC_USDT-2h.json');
 
 function main() {
   const kline = ExpandKLine(ArrayToKLine(data, false), 0.5);
-  const executor = new SpotSimpleTest(100, -0.001);
-  const bot = new MAKD(executor, {
-    fast_period: 15,
-    slow_period: 29,
-    k_period: 4,
-    d_period: 7,
+  const executor = new SpotSimpleTest(100, 0.001);
+  const bot = new SMACross(executor, {
+    fast_period: 10,
+    slow_period: 20,
     stop_rate: 1,
     take_rate: 1e6,
   });
   bot.BackTestingBatch(kline);
   console.log(executor.ROI(kline[kline.length - 1].close));
-  console.log(executor.ROINet(kline[kline.length - 1].close));
+  // console.log(executor.ROINet(kline[kline.length - 1].close));
 }
 
 main();
