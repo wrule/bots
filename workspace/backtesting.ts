3b450850a6f69f639dd8daaa626ed131fc5b8452
jimao <jimao@jimaodeMacBook-Pro.local>
2023-02-01 15:47:07 +0800

    Slice方法

--- workspace/backtesting.ts
+++ workspace/backtesting.ts
@@ -1,15 +1,23 @@
 #!/usr/bin/env node
-import { ArrayToKLine, ExpandKLine, SpotSimpleTest } from 'litebot';
+import { ArrayToKLine, ExpandKLine, SpotSimpleTest, TC } from 'litebot';
+import moment from 'moment';
 import { SMACross } from '../sma_cross';
 
-const data = require('../data/BTC_USDT-2h.json');
+const data = require('../data/ETH_USDT-2h.json');
+
+export
+function Slice<T extends TC>(tcs: T[], start?: any, end?: any) {
+  const start_timestamp = start != null ? moment(start).valueOf() : -Infinity;
+  const end_timestamp = end != null ? moment(end).valueOf() : Infinity;
+  return tcs.filter((tc) => tc.time >= start_timestamp && tc.time <= end_timestamp);
+}
 
 function main() {
-  const kline = ExpandKLine(ArrayToKLine(data, false), 0.5);
+  const kline = Slice(ExpandKLine(ArrayToKLine(data, false), 0.5), '2009', '2026');
   const executor = new SpotSimpleTest(100, 0.001);
   const bot = new SMACross(executor, {
-    fast_period: 10,
-    slow_period: 20,
+    fast_period: 9,
+    slow_period: 44,
     stop_rate: 1,
     take_rate: 1e6,
   });
