2f39dabe23aaa8d6e30a619e8c757636b39b340f
jimao <jimao@jimaodeMacBook-Pro.local>
2023-05-06 14:34:39 +0800

    makd_fp

--- makd_fp.ts
+++ makd_fp.ts
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
-import { Bot, SpotFull, TC, t, FillParams, DingTalk, ccxt, SpotReal, KLineWatcher, OHLCV, KLineWatcherRT, ExFactory } from 'litebot';
+import { Bot, SpotFull, TC, t, FillParams, DingTalk, ccxt, SpotRealFeePool, KLineWatcher, OHLCV, KLineWatcherRT, ExFactory } from 'litebot';
 import moment from 'moment';
 
 export
@@ -98,6 +98,8 @@ extends Bot<TC, Signal> {
     take_rate: 1e6,
     funds: 11,
     assets: 0,
+    fee: 0.00075,
+    fee_pool: 0,
     final_price: NaN,
     last_action: '',
     init_valuation: NaN,
@@ -110,7 +112,7 @@ extends Bot<TC, Signal> {
   const exchange = ExFactory(secret.exchange);
   console.log('loading market...');
   await exchange.loadMarkets();
-  const executor = new SpotReal({ exchange, notifier, ...params });
+  const executor = new SpotRealFeePool({ exchange, notifier, ...params });
   const bot = new MAKD(executor, params);
   (params.rt ? new KLineWatcherRT() : new KLineWatcher(params.countdown)).RunBot({ exchange, bot, ...params });
 })();
