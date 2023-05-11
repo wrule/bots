6c351903dc9c7ca1c2ba9084fc7e6c123ec0324a
jimao <jimao@jimaodeMacBook-Pro.local>
2023-05-11 18:54:17 +0800

    提交代码

--- charger_fp.ts
+++ charger_fp.ts
@@ -4,35 +4,39 @@ import { ExFactory, FillParams } from "litebot";
 const secret = require('./.secret.json');
 const exchange = ExFactory({ ...secret.exchange });
 
-async function check() {
+async function check(params: any) {
   try {
     const balance = await exchange.fetchFreeBalance();
-    console.log(balance['ETH']);
+    console.log(balance[params.asset]);
   } catch (e) {
     console.log(e);
   }
-  setTimeout(() => check(), 5000);
+  setTimeout(() => check(params), params.interval);
 }
 
 async function main() {
-  const params = {
+  const params: any = {
     symbol: 'BNB/USDT',
-    less_than: 30,
-    amount: 12,
+    less_than: 22,
+    amount: 11,
+    interval: 6 * 60 * 60 * 1000,
   };
   FillParams(params);
   if (!/^\S+\/\S+$/.test(params.symbol)) {
     params.symbol = `${params.symbol}/USDT`;
     console.log(params);
   }
-  const [token_asset, token_fund] = params.symbol.split('/');
+  const [asset, fund] = params.symbol.split('/');
+  params.asset = asset;
+  params.fund = fund;
   const ticker = await exchange.fetchTicker(params.symbol);
-  const less_than_dst = params.less_than / ticker.ask;
-  const amount_dst = params.amount / ticker.ask;
-  console.log(token_asset, '当前价格: ', ticker.ask, token_fund);
-  console.log('以当前价格确定的持仓基线', less_than_dst, token_asset);
-  console.log('以当前价格确定的单笔追加额', amount_dst, token_asset);
-  // check();
+  params.less_than_dst = params.less_than / ticker.ask;
+  params.amount_dst = params.amount / ticker.ask;
+  console.log(asset, '当前价格: ', ticker.ask, fund);
+  console.log('以当前价格确定的持仓基线', params.less_than_dst, asset);
+  console.log('以当前价格确定的单笔追加额', params.amount_dst, asset);
+  console.log(params);
+  check(params);
 }
 
 main();
