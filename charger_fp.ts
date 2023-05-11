fa4fe0c2a0b533914b952aa957481c07e3bf3de4
jimao <jimao@jimaodeMacBook-Pro.local>
2023-05-11 18:07:11 +0800

    提交代码

--- charger_fp.ts
+++ charger_fp.ts
@@ -1,2 +1,21 @@
 #!/usr/bin/env node
+import { ExFactory } from "litebot";
 
+const secret = require('./.secret.json');
+const exchange = ExFactory({ ...secret.exchange });
+
+async function check() {
+  try {
+    const balance = await exchange.fetchFreeBalance();
+    console.log(balance['ETH']);
+  } catch (e) {
+    console.log(e);
+  }
+  setTimeout(() => check(), 5000);
+}
+
+function main() {
+  check();
+}
+
+main();
