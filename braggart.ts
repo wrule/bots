94c6316798e6ba934f7d25a1b66ce968eaa2a26f
jimao <jimao@jimaodeMacBook-Pro.local>
2023-05-12 14:08:52 +0800

    提交代码

--- braggart.ts
+++ braggart.ts
@@ -3,12 +3,16 @@ import fs from 'fs';
 import { createInterface } from 'readline';
 
 async function loadBigJsonArray(path: string) {
+  console.log(`start reading ${path}`);
   const result: any[] = [];
   const readline = createInterface({
     input: fs.createReadStream(path),
     crlfDelay: Infinity,
   });
+  let count = 0;
   for await (let line of readline) {
+    count++;
+    if (count % 100000 === 1) console.log('rows: ', count);
     line = line.trim();
     if (line) {
       if (line.endsWith(','))
@@ -17,7 +21,7 @@ async function loadBigJsonArray(path: string) {
       result.push(row);
     }
   }
-  console.log('文件读取完毕');
+  console.log('file read complete');
   return result;
 }
 
